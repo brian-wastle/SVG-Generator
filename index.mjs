2d73c4afaa59e7c4fdac92ac93a3ebb39ac4d6eb
brian-wastle <brian.wastle1@gmail.com>
2023-08-17 22:52:32 -0400

    added inquirer prompts

---
+++ index.mjs
@@ -0,0 +1,60 @@
+import inquirer from 'inquirer';
+import fs from "fs/promises";
+
+let {shapeInput, bgColorInput, svgTextInput, textColorInput } = await inquirer
+    .prompt([
+        {
+            type: 'list',
+            message: 'Choose a logo shape from the following: ',
+            name: 'shapeInput',
+            choices: ['Circle', 'Square', 'Triangle'],
+        },
+        {
+            type: 'input',
+            name: 'bgColorInput',
+            message: "Enter a color for your shape (color name or hexidecimal color code): ",
+            default() {
+                return 'red';
+                },
+        },
+        {
+            type: 'input',
+            name: 'svgTextInput',
+            message: "Enter your logo text (up to 3 letters or numbers): ",
+            validate(value) {
+                const pass = /^[A-Z]{0,3}$/i.test(value);
+                if (pass) {
+                    return true;
+                    }
+                return 'Please enter up to 3 letters or numbers.  Use the backspace key.';
+                },
+        },
+        {
+            type: 'input',
+            name: 'textColorInput',
+            message: "Enter your text color: ",
+            default() {
+                return 'white';
+                },
+        }, 
+  ])
+  .then((data) => {
+    
+
+  });
+
+
+
+
+
+
+
+
+
+// FileSystem.writeFile( "shape.svg", example.markUp, function(err){
+//     writeFile('message.txt', data, (err) => {
+//         if(err) {
+//         console.log(err);
+//         }
+//     })
+// })
